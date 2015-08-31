49880b4de5236bc8907c0981ca00bb76a862251d
Adam J <snoopyk921+proman21gh@gmail.com>
2015-08-31 12:28:43 +1000

    Provide basic skeleton

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname + "/src/app",
+  entry: {
+    javascript: "./main.js",
+    html: "./index.html",
+  },
+  output: {
+    path: __dirname + "/build/app",
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      }
+    ]
+  }
+}
