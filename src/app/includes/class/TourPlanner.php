8e92aea96b9b2d2d83f9d9ccb73971ef7eb2d85f
Liam Morrow <ldmorrow@outlook.com>
2015-09-03 08:44:36 +1000

    Added the skeleton for the TourPlanner Class

---
+++ src/app/includes/class/TourPlanner.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liam
+ * Date: 03/09/2015
+ * Time: 8:38 AM
+ */
+class TourPlanner {
+    /**
+     * Initializes the TourPlanner Object
+     */
+    function TourPlanner(){
+
+    }
+
+    /**
+     * Returns an array of PlaceOfInterest that has been cross checked with trove and the google places API
+     * @param $location string
+     * @param $radius int
+     * @param $interest string
+     * @returns array
+     */
+    function getPlaceList($location, $radius, $interest) {
+        die("Not Implemented");
+    }
+}
