5d7519d48960434355806182338ebdfaa0620f9a
Adam J <snoopyk921+proman21gh@gmail.com>
2015-09-07 18:59:54 +1000

    Add Homepage js

--- src/app/GuidrApp.js
+++ src/app/GuidrApp.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import {Component} from "react";
+import Homepage from "./homepage/components/Homepage";
 
-export default class GuidrApp extends React.Component {
-  
+export default class GuidrApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (<Homepage />);
+  }
 }
---
+++ src/app/homepage/components/Homepage.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    
+  }
+}
