6731dfeabdd4f08fb6ce0016d8524b55d9bff85d
Carl Foster <carlf1996@bigpond.com>
2015-09-21 10:55:18 +1000

    Add support for displaying location information on map

--- src/app/map/components/Map.js
+++ src/app/map/components/Map.js
@@ -3,6 +3,14 @@ import {GoogleMap, Marker} from "react-google-maps";
 import Controls from "./Controls";
 import InfoWindow from "./InfoWindow";
 
+const geolocation = (
+    "undefined" !== typeof window && navigator && navigator.geolocation || {
+        getCurrentPosition: (success, failure) => {
+            failure("Your browser does not support geolocation.");
+        },
+    }
+);
+
 export default class Map extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +19,14 @@ export default class Map extends Component {
 
   render() {
     let overlay;
+
+    state = {
+        marker: null,
+        directions: null,
+        user: null,
+        place: null,
+    };
+    
     if(!this.props.background) {
       overlay = (<div className="overlay">
                   <Controls />
@@ -31,7 +47,49 @@ export default class Map extends Component {
         zoomControl={false}
         scaleControl={false}
         streetViewControl={false}>
+        {directions ? <DirectionsRenderer directions={directions} /> : null}
+        {place ? <Marker {...marker} /> : null}
       </GoogleMap>
     </div>);
   }
+
+  createMarker = () => {
+      var details = {
+          position: state.place.geometry.location,
+          key: state.place.name
+      }
+      this.setState({ marker: details });
+  }
+
+  updateUserLocation = (position) {
+      var userLoc = new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+              );
+      this.setState({ user: userLoc });
+      showDirections();
+  }
+
+  setPlace = (place) {
+      this.setState({ place: place });
+  }
+
+  showDirections = () => {
+      const directionsService = new google.maps.DirectionService();
+        request = {
+          origin: state.user,
+          destination: state.place.geometry.location,
+          travelMode: google.maps.TravelMode.WALKING
+        }
+      };
+
+      directionsService.route(request, (result, status) => {
+          if (status == google.maps.DirectionsStatus.OK) {
+              this.setState({ directions: result });
+          }
+      });
+  }
+
+  navigator.watchPosition(updateUserLocation);
+
 }
